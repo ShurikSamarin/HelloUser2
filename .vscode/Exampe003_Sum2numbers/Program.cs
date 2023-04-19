b538abda3fbf4a5c465ea99f448b759e13ce1a26
Alexander <sttp2002@gmail.com>
2023-04-19 12:20:13 +0300

    add random

--- .vscode/Exampe003_Sum2numbers/Program.cs
+++ .vscode/Exampe003_Sum2numbers/Program.cs
@@ -1,6 +1,6 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int numberA = 31;
+int numberA = new Random().Next(1,10);
 int numberB = 5;
 int result = numberA + numberB;
 Console.WriteLine(result);
