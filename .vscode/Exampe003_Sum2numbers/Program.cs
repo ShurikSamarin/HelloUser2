b207b7f0ab17ed03e79fb22714ebb701bac03184
Alexander <sttp2002@gmail.com>
2023-04-19 12:00:58 +0300

    add divide

---
+++ .vscode/Exampe003_Sum2numbers/Program.cs
@@ -0,0 +1,6 @@
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
