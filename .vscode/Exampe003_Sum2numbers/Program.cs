8d99b68076196ed4526e51d9312cf8ad3abb7776
Alexander <sttp2002@gmail.com>
2023-04-19 14:32:20 +0300

    add weight

--- .vscode/Exampe003_Sum2numbers/Program.cs
+++ .vscode/Exampe003_Sum2numbers/Program.cs
@@ -2,5 +2,7 @@
 Console.WriteLine("Hello, World!");
 int numberA = new Random().Next(1,10);
 int numberB = 5;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
